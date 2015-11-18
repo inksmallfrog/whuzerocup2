0def2d4b645851c6ab044d57a7b17bde4f0fbf69
inksmallfrog <inksmallfrog@gmail.com>
2015-11-18 19:37:53 +0800

    ignore something

--- backend/DBConnector.php
+++ backend/DBConnector.php
@@ -16,7 +16,7 @@ class DBConnector
     }
 
     private function createConnection(){
-              $this->conn = mysqli_connect(SAE_MYSQL_HOST_M, SAE_MYSQL_USER, SAE_MYSQL_PASS, SAE_MYSQL_DB, SAE_MYSQL_PORT);
+              $this->conn = mysqli_connect("127.0.0.1", "root", "123456", "LoverSpaceInformation", "3306");
         if(!conn){
             echo "连接数据库失败".mysqli_error();
         }
